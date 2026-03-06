62b838f0100f890fb18efb39d631cbf7cb402230
Sher Hassan <sherhassan739@gmail.com>
2026-03-06 10:26:35 +0500

    tst

--- SearchLibrary/src/test/java/com/sqe/search/BinarySearchTest.java
+++ SearchLibrary/src/test/java/com/sqe/search/BinarySearchTest.java
@@ -19,11 +19,16 @@ public class BinarySearchTest {
 
     @Test
     void testFoundMiddle() {
-        assertEquals(2, bs.search(new int[]{1,2,3,4,5}, 3));
+        assertEquals(2, bs.search(new int[]{1,2,3,4,5}, 5));
     }
 
     @Test
     void testNotFound() {
         assertEquals(-1, bs.search(new int[]{1,2,3,4,5}, 10));
     }
+
+    @Test
+    void testBreak() {
+    assertEquals(999, bs.search(new int[]{1,2,3}, 2));
+}
 }
